34e6c937595aad0981703632d68dd65c48ea5f05
neydor <neydo@DESKTOP-R6FN7VV>
2025-07-17 23:31:40 -0500

    Inicializacion basica

---
+++ CatalogoDeProductosAranda/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using CatalogoDeProductosAranda.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CatalogoDeProductosAranda.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private Producto[] productos = new Producto[]
+        {
+            new Producto { Id = 1, Name = "Product 1" },
+            new Producto { Id = 2, Name = "Product 2" },
+            new Producto { Id = 3, Name = "Product 3" }
+        };
+        public ProductsController() { }
+
+        public IEnumerable<Producto> GetAllProducts()
+        {
+            return productos;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = productos.FirstOrDefault(p => p.Id == id);
+            if(productos.Any() ) {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
